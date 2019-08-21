cb9d5a0f17564a7ad4c057e89439f10f62c26658
APCS2nd <APCS2nd@MHS-LT9-21>
2019-08-21 12:16:58 -0700

    Complete Hello World Project

---
+++ Hello World Project/src/HelloWorld2.java
@@ -0,0 +1,16 @@
+//Chloe Xu
+//8/19/19
+//first class (iconic) for AP CS A
+
+
+
+ public class HelloWorld2 {
+ 
+
+	public static void main(String[] args) {
+      System.out.print("Hello World!");
+      System.out.println("Hello World!");
+
+	}
+
+}
