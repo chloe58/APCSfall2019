8b421e879d1bededd2a2bf4d4ac5fea42df33676
APCS2nd <APCS2nd@MHS-LT9-21>
2019-09-04 12:02:58 -0700

    Access Calculate

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,16 @@
+/*Chloe Xu
+ * August 29, 2019
+ * Period 4
+ * Call various math functions from the Calculate library
+ * Client code
+ */
+public class DoMath {
+	
+	public static void main(String[]args) {
+		System.out.println(Calculate.square(-12));
+		System.out.println(Calculate.cubel(-12));
+
+		
+	}
+
+}
