2ff1c5969f0287fded39ee2c21b0cc2de9b42c65
chloexu <chloexu@chloes-air>
2019-11-25 11:02:43 -0800

    11/

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -4,22 +4,21 @@
  * Group of Methods that perform math calculations (a library)
  */
 public class Calculate {
-	// a call to square returns the square of the value passed
-	
+	//returns the square of the value passed
 	public static int square(int number) {
 		int squared=0; //initialize answer
 		squared = number * number; //multiply input by itself
 		return squared;
 	}
 	
-	//a call to cube returns the square of the value passed
+	//returns the square of the value passed
 	public static int cube(int number) {
 		int cubed=0; 
 		cubed = number * number * number; //multiply input by itself twice
 		return cubed;
 	}
 	
-	//a call to average returns the average of the value passed
+	//returns the average of the value passed
 	public static double average(double number1, double number2) {
 		double answer=0; 
 		answer = ((number1 + number2)/2); //add the two inputs then divide the total by 2
@@ -33,42 +32,42 @@ public class Calculate {
 		return avg;
 	}
 	
-	//a call to toDegrees converts an angle measure given in radians into degrees
+	//converts an angle measure given in radians into degrees
 	public static double toDegrees(double rad) {
-		double degree=0;
-		degree = (rad * (180/3.14159)); //using a ratio, convert to  degrees 
+		double degree = 0;
+		degree = (rad * (180/3.14159)); 
 		return degree;
 	}
 	
-	//a call toRadians converts an angle measure given in degrees into radians
+	//converts an angle measure given in degrees into radians
 	public static double toRadians(double degree) {
-		double rad=0;
-		rad = (degree * (3.14159/180)); //using a ratio, convert to radians
+		double rad = 0;
+		rad = (degree * (3.14159/180)); 
 		return rad;
 	}
 	
-	//a call to discriminant provides the coefficients of a quadratic equation in standard form (a, b, and c) and returns the value of the discriminant
+	//provides the coefficients of a quadratic equation in standard form (a, b, and c) and returns the value of the discriminant
 	public static double discriminant(double a, double b, double c) {
-		double discriminant=0;
-		discriminant = ((b * b) + (-4 * a * c)); 
-		return discriminant;
+		double discr = 0;
+		discr = ((b * b) + (-4 * a * c)); 
+		return discr;
 	}
 	
-	//a call toImproperFrac converts mixed number (with its pieces provided separately in the whole number, numerator, then denominator) into an improper fraction
+	//converts mixed number (with its pieces provided separately in the whole number, numerator, then denominator) into an improper fraction
 	public static String toImproperFrac (int whole, int num, int den) {
-		int mixednum=0; 
+		int mixednum = 0; 
 		mixednum = ((whole * den) + num);
 		return mixednum + "/" + den;
 	}
 	
-	//a call toMixedNum converts an improper fraction (with its pieces provided separately in the order numerator then denominator)into a mixed number
+	//converts an improper fraction (with its pieces provided separately in the order numerator then denominator)into a mixed number
 	public static String toMixedNum(int num, int den) {
 		int whole = num/den;
-		int rem =  num%den;
+		int rem = num%den;
 		return whole + "_" + rem + "/" + den;
 	}
 	
-	//a call to foil converts a binomial multiplication of the form (ax + b)(cx+d) into quadratic equation of the form ax^2 + bx + c, accepts a four integers and a String and then returns a String.
+	//converts a binomial multiplication of the form (ax + b)(cx+d) into quadratic equation of the form ax^2 + bx + c, accepts a four integers and a String and then returns a String.
 	public static String foil(int a, int b, int c, int d, String var) {
 		int A = a*c;
 		int B = (a*d)+(b*c);
@@ -76,7 +75,7 @@ public class Calculate {
 		return A + var + "^2" + " " + "+" + " " + B + "n" + " " + "+" + " " + C;
 	}
 	
-	//a call to isDivisibleBy determines whether or not one integer is evenly divisible by another, accepts two integers and returns a boolean.
+	//determines whether or not one integer is evenly divisible by another, accepts two integers and returns a boolean.
 	public static boolean isDivisibleBy(int number1, int number2) {
 		if(number1%number2==0) {
 			return true;
@@ -85,7 +84,7 @@ public class Calculate {
 		}
 	}
 	
-	//a call to absValue returns the absolute value of the number passed, method accepts a double and returns a double.
+	//returns the absolute value of the number passed, method accepts a double and returns a double.
 	public static double absValue(double number1) {
 		if (number1<0) { 
 			return number1*-1;
@@ -94,7 +93,7 @@ public class Calculate {
 		}
 	}
 	
-	//a call to max returns the larger of the values passed, accepts two doubles and returns a double.
+	//returns the larger of the values passed, accepts two doubles and returns a double.
 	public static double max(double number1, double number2) {
 		if (number1>=number2) {
 			return number1;
@@ -114,16 +113,23 @@ public class Calculate {
 		}
 	}
 	
-	//a call to min returns the smaller of the values passed, accepts two integers and returns an int.
-	public static double min(double root1, double root2) {
-		if (root1<=root2) {
-			return root1;
+	//returns the smaller of the values passed, accepts two integers and returns an int.
+	public static double min(double number1, double number2) {
+		if (number1<=number2) {
+			return number1;
 		} else {
-			return root2;
+			return number2;
 		}
 	}
-	
-	//a call to round2 rounds a double correctly to 2 decimal places and returns a double
+	//int
+	public static int min(int number1, int number2) {
+		if (number1<=number2) {
+			return number1;
+		} else {
+			return number2;
+		}
+	}
+	//rounds a double correctly to 2 decimal places and returns a double
 	public static double round2(double decimal) {
 		double rounded=decimal;
 		rounded = rounded *100;
@@ -137,24 +143,24 @@ public class Calculate {
 		return rounded;
 	}
 	
-	//a call to exponent raises a value to a positive integer power, accepts a double and an integer and returns a double, exponent is positive 
-	public static double exponent(double value, int exp) {
+	//raises a value to a positive integer power, accepts a double and an integer and returns a double
+	public static double exponent(double value, int power) {
 		double answer = value;
-		if (exp<0) {
+		if (power<0) {
 			throw new ArithmeticException("Invalid Argument");
 		}
-			for (int i = 0; i < exp - 1; i++) {
+			for (int i = 0; i < power - 1; i++) {
 				answer *= value;
 			} return answer;
 	}
 	
-	//a call to factorial returns the factorial of the value passed, accepts an integer and returns an integer, exponent is positive
+	//returns the factorial of the value passed, accepts an integer and returns an integer
 	public static int factorial(int num1) {
-		int result = 1;
+		int fact = 1;
 			for (int i=1; i<=num1; i++) {
-				result*= i;
+				fact*= i;
 			}
-		return result;
+		return fact;
 	}
 	
 	//a call to isPrime determines whether or not an integer is prime, accepts an integer and returns a boolean
@@ -178,7 +184,7 @@ public class Calculate {
 		return output;
 	}
 	
-	//a call to sqrt returns an approximation of the square root, accepts a double and returns a double, rounds to hundredth place
+	//returns an approximation of the square root, accepts a double and returns a double, rounds to hundredth place
 	public static double sqrt(double num) {
 		if(num<0) {
 			throw new ArithmeticException("Invalid Argument");
